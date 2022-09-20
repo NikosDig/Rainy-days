c880f626697857f847f960307430a83829dae857
NikosDigalakis <nikos_23_chania@hotmail.com>
2022-09-20 19:52:46 +0200

    minor adjustment to hamburger

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
+const main = document.querySelector("main");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   hamburgerMenu.classList.toggle("active");
+});
+
+main.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  hamburgerMenu.classList.remove("active");
 });
