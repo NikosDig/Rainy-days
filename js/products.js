7c939d53bdc94147221b66852889254471a4702e
NikosDigalakis <nikos_23_chania@hotmail.com>
2022-09-27 19:36:02 +0200

    fixed some functionality to cart nav

---
+++ js/products.js
@@ -0,0 +1,38 @@
+export const productList = [
+  {
+    name: "follow jacket",
+    id: "1",
+    price: 7000,
+    inCart: 0,
+  },
+  {
+    name: "greenland jacket",
+    id: "2",
+    price: 10000,
+    inCart: 0,
+  },
+  {
+    name: "holmlia jacket",
+    id: "3",
+    price: 200000,
+    inCart: 0,
+  },
+  {
+    name: "oslo jacket",
+    id: "4",
+    price: 370000,
+    inCart: 0,
+  },
+  {
+    name: "sarp jacket",
+    id: "5",
+    price: 170000,
+    inCart: 0,
+  },
+  {
+    name: "unfollow jacket",
+    id: "6",
+    price: 1000,
+    inCart: 0,
+  },
+];
--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+import { productList } from "./products.js";
 const hamburger = document.querySelector(".hamburger");
 const hamburgerMenu = document.querySelector(".hamburgerMenu");
 const main = document.querySelector("main");
+const addToCartButton = document.querySelector(".cart");
+const numberOfItemsInWallet = document.querySelector(".numberOfItemsInWallet");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -11,3 +14,35 @@ main.addEventListener("click", () => {
   hamburger.classList.remove("active");
   hamburgerMenu.classList.remove("active");
 });
+
+//  cart functionality
+function checkIfButtonExists() {
+  if (addToCartButton) {
+    addToCartButton.addEventListener("click", () => {
+      cartNumbers();
+      walletNumberOnMemory();
+    });
+  }
+}
+
+function walletNumberOnMemory() {
+  let productsTotal = localStorage.getItem("cartNumbers");
+  if (productsTotal) {
+    numberOfItemsInWallet.textContent = productsTotal;
+    numberOfItemsInWallet.style.display = "inline-block";
+  }
+}
+
+function cartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+  productNumbers = parseInt(productNumbers);
+  if (productNumbers) {
+    localStorage.setItem("cartNumbers", productNumbers + 1);
+    numberOfItemsInWallet.textContent = productNumbers + 1;
+  } else {
+    localStorage.setItem("cartNumbers", 1);
+    numberOfItemsInWallet.textContent = 1;
+  }
+}
+checkIfButtonExists();
+walletNumberOnMemory();
